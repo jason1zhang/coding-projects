a96cd7f85db67c75ebb223e71a0e9d804197bc53
jason1zhang <jason1_zhang@hotmail.com>
2023-05-25 08:51:20 +0800

    Leet code #122
    Best time to buy and sell stock II

--- leetcode_python/LCArray.py
+++ leetcode_python/LCArray.py
@@ -1,6 +1,24 @@
 from typing import List
 
 class LCArray:
+    @staticmethod
+    def max_profit_II(prices: List[int]) -> int:
+        """
+        Leet code # 122
+
+        You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+        On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of
+        the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+        Find and return the maximum profit you can achieve.
+        """
+        # Greedy algorithm
+        max_profit = 0
+        for i in range(1, len(prices)):
+            max_profit += max(0, prices[i] - prices[i - 1])
+
+        return max_profit
 
     @staticmethod
     def max_profit_1(prices: List[int]) -> int:
--- leetcode_python/main.py
+++ leetcode_python/main.py
@@ -3,5 +3,5 @@ from LCArray import LCArray
 
 if __name__ == '__main__':
     prices = [7,1,5,3,6,4]
-    print(LCArray.max_profit_2(prices))
+    print(LCArray.max_profit_II_1(prices))
 
