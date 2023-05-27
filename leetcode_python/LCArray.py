f972d359e0a81352d496960f4991618a9740df8c
jason1zhang <jason1_zhang@hotmail.com>
2023-05-27 09:41:10 +0800

    Leet code #45
    use greedy algorithm to solve leet code # 45 Jump Game II

--- leetcode_python/LCArray.py
+++ leetcode_python/LCArray.py
@@ -2,6 +2,33 @@ from typing import List
 
 class LCArray:
     @staticmethod
+    def jump(nums: List[int]) -> int:
+        """
+        Leet code # 45
+
+        You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+
+        Each element nums[i] represents the maximum length of a forward jump from index i. In other words,
+        if you are at nums[i], you can jump to any nums[i + j] where:
+            - 0 <= j <= nums[i] and
+            - i + j < n
+
+        Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can
+        reach nums[n - 1].
+        """
+        # Greedy algorithm
+        n, max_pos, end, step = len(nums), 0, 0, 0
+        for i in range(n - 1):
+            if max_pos >= i:
+                max_pos = max(max_pos, i + nums[i])
+                if i == end:
+                    end = max_pos
+                    step += 1
+
+        return step
+
+
+    @staticmethod
     def can_jump_1(nums: List[int]) -> bool:
         """
         Leet code # 55
--- leetcode_python/main.py
+++ leetcode_python/main.py
@@ -3,5 +3,5 @@ from LCArray import LCArray
 
 if __name__ == '__main__':
     nums = [2,3,1,1,4]
-    print(LCArray.can_jump_3(nums))
+    print(LCArray.jump(nums))
 
