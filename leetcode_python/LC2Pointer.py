1d0274b5c38f435f880cff37289192d52a263844
jason1zhang <jason1_zhang@hotmail.com>
2023-07-10 06:31:26 +0800

    Leet code # 392
    solve leet code # 392

--- leetcode_python/LC2Pointer.py
+++ leetcode_python/LC2Pointer.py
@@ -3,6 +3,28 @@
 class LC2Pointer:
 
     @staticmethod
+    def is_subsequence(s: str, t: str) -> bool:
+        """
+        Leet code # 392
+        Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+        A subsequence of a string is a new string that is formed from the original string
+        by deleting some (can be none) of the characters without disturbing the relative positions
+        of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+        """
+        n, m = len(s), len(t)
+        i = j = 0
+
+        while i < n and j < m:
+            if s[i] == t[j]:
+                i += 1
+
+            j += 1
+
+        return i == n
+
+
+    @staticmethod
     def is_palindrome_2(s: str) -> bool:
         """
         Approach 2 with 2 pointers
--- leetcode_python/main.py
+++ leetcode_python/main.py
@@ -4,8 +4,9 @@ from LC2Pointer import LC2Pointer
 
 
 if __name__ == '__main__':
-    s = "A man, a plan, a canal: Panama"
-    print(LC2Pointer.is_palindrome_2(s))
+    s = "abc"
+    t = "ahbgdc"
+    print(LC2Pointer.is_subsequence(s, t))
 
 
 
