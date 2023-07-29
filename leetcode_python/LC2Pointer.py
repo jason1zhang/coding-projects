38cecd67054ce017ff5fdf2672add987449064b1
jason1zhang <jason1_zhang@hotmail.com>
2023-07-29 10:51:42 +0800

    leet code #167
    two sum problem with the sorted array

--- leetcode_python/LC2Pointer.py
+++ leetcode_python/LC2Pointer.py
@@ -3,6 +3,35 @@
 class LC2Pointer:
 
     @staticmethod
+    def two_sum_II(numbers: list[int], target: int) -> list[int]:
+        """
+        Leet code # 167
+        Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order,
+        find two numbers such that they add up to a specific target number.
+
+        Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 < numbers.length.
+
+        Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+        The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+        Your solution must use only constant extra space.
+        """
+        low, high = 0, len(numbers) - 1
+
+        while low < high:
+            total = numbers[low] + numbers[high]
+            if total == target:
+                return [low + 1, high + 1]
+            elif total < target:
+                low += 1
+            else:
+                high -= 1
+
+        return [-1, -1]
+
+
+    @staticmethod
     def is_subsequence(s: str, t: str) -> bool:
         """
         Leet code # 392
--- leetcode_python/main.py
+++ leetcode_python/main.py
@@ -4,9 +4,9 @@ from LC2Pointer import LC2Pointer
 
 
 if __name__ == '__main__':
-    s = "abc"
-    t = "ahbgdc"
-    print(LC2Pointer.is_subsequence(s, t))
+    numbers = [2,7,11,15]
+    target = 9
+    print(LC2Pointer.two_sum_II(numbers, target))
 
 
 
