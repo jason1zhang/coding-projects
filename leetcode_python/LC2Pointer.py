83d61332c851ea1a0326f2ccb9a0eff6787f44d2
jason1zhang <jason1_zhang@hotmail.com>
2023-07-30 10:48:29 +0800

    leet code # 11
    container with most water

--- leetcode_python/LC2Pointer.py
+++ leetcode_python/LC2Pointer.py
@@ -1,6 +1,31 @@
 
 
 class LC2Pointer:
+    @staticmethod
+    def max_area(height: list[int]) -> int:
+        """
+        You are given an integer array height of length n. There are n vertical lines drawn
+        such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+        Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+        Return the maximum amount of water a container can store.
+
+        Notice that you may not slant the container.
+        """
+        left, right = 0, len(height) - 1
+        answer = 0
+
+        while left < right:
+            area = min(height[left], height[right]) * (right - left)
+            answer = max(answer, area)
+
+            if height[left] <= height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return answer
 
     @staticmethod
     def two_sum_II(numbers: list[int], target: int) -> list[int]:
--- leetcode_python/main.py
+++ leetcode_python/main.py
@@ -4,9 +4,8 @@ from LC2Pointer import LC2Pointer
 
 
 if __name__ == '__main__':
-    numbers = [2,7,11,15]
-    target = 9
-    print(LC2Pointer.two_sum_II(numbers, target))
+    height = [1,8,6,2,5,4,8,3,7]
+    print(LC2Pointer.max_area(height))
 
 
 
