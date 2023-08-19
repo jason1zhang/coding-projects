d4bd5ad0b8cbf4756c8d684907cd91b86c81c124
jason1zhang <jason1_zhang@hotmail.com>
2023-08-19 09:12:25 +0800

    leet code # 3
    Given a string s, find the length of the longest substring without repeating characters.

--- leetcode_python/LCSlidingWindow.py
+++ leetcode_python/LCSlidingWindow.py
@@ -1,4 +1,29 @@
 class LCSlidingWindow:
+
+    @staticmethod
+    def len_of_longest_substring(s: str) -> int:
+        """
+        Leet code # 3
+        Given a string s, find the length of the longest substring without repeating characters.
+        """
+        seen = set()  # hashset to judge whether a char has occurred before
+        n = len(s)
+
+        right = -1  # right pointer, initially pointing to the right of the beginning
+        answer = 0
+
+        for i in range(n):
+            if i != 0:
+                seen.remove(s[i - 1])
+
+            while (right + 1) < n and s[right + 1] not in seen:
+                seen.add(s[right + 1])
+                right += 1
+
+            answer = max(answer, right - i + 1)
+
+        return answer
+
     @staticmethod
     def min_sub_array_len(target: int, nums: list[int]) -> int:
         """
--- leetcode_python/main.py
+++ leetcode_python/main.py
@@ -5,9 +5,8 @@ from LCSlidingWindow import LCSlidingWindow
 
 
 if __name__ == '__main__':
-    target = 7
-    nums = [2,3,1,2,4,3]
-    print(LCSlidingWindow.min_sub_array_len(target, nums))
+    s = "pwwkew"
+    print(LCSlidingWindow.len_of_longest_substring(s))
 
 
 
