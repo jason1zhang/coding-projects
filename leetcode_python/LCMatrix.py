8d6b09750999804a4317657df167531ba8099eb8
jason1zhang <jason1_zhang@hotmail.com>
2023-09-03 11:16:45 +0800

    Leet code #54
    Given an m x n matrix, return all elements of the matrix in spiral order.

--- leetcode_python/LCMatrix.py
+++ leetcode_python/LCMatrix.py
@@ -2,6 +2,39 @@ from typing import List
 
 
 class LCMatrix:
+
+    @staticmethod
+    def spiral_order(matrix: List[List[int]]) -> List[int]:
+        """
+        Leet code # 54
+
+        Given an m x n matrix, return all elements of the matrix in spiral order.
+        """
+        if not matrix or not matrix[0]:
+            return list()
+
+        rows, columns = len(matrix), len(matrix[0])
+        visited = [[False] * columns for _ in range(rows)]
+        total = rows * columns
+        order = [0] * total
+
+        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        row, column = 0, 0
+        directionIndex = 0
+
+        for i in range(total):
+            order[i] = matrix[row][column]
+            visited[row][column] = True
+            nextRow, nextColumn = row + directions[directionIndex][0], column + directions[directionIndex][1]
+
+            if not(0 <= nextRow < rows and 0 <= nextColumn < columns and not visited[nextRow][nextColumn]):
+                directionIndex = (directionIndex + 1) % 4
+
+            row += directions[directionIndex][0]
+            column += directions[directionIndex][1]
+
+        return order
+
     @staticmethod
     def is_valid_sudoku(board: List[List[str]]) -> bool:
         """
